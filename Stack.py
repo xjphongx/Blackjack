0d473947292b617337929185889afea2648e7203
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-08-08 10:46:16 -0700

    fixed file typo

---
+++ Stack.py
@@ -0,0 +1,16 @@
+
+class Stack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self):
+        pass
+    def pop(self):
+        pass
+    def top(self):
+        pass
+    
+    ##add a access(self) function 
+
+   
