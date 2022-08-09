0f222abe4b5a551f5406fa7c0f05c30659f3a2a3
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-08-08 22:41:58 -0700

    finished adding Card objects into a Stack

--- Card.py
+++ Card.py
@@ -1,29 +1,46 @@
 from Stack import Stack ## from Stack.py file, import and use the Stack class
-import json
+import json ##doing my testing in Card.py file
 class Card:
 
-    def __init__(self, card_type,pip_value, suit):
+    def __init__(self, card_type, pip_value, suit):
         self.card_type = card_type
         self.pip_value = pip_value
         self.suit = suit
-        self.low_pip_value=0        #used for Aces
-        self.high_pip_value=0       #used for Aces
-        ##self.value = 0 ## this will change 
-
-
-
-
-
-
+        if(card_type) == "Ace":
+            self.low_pip_value=1         #used for Aces
+            self.high_pip_value=11       #used for Aces
+        
+    def get_card_type(self):
+        return self.card_type
 
 
 
 
 
 ## testing area
-testCard = Card(5,"Spade")
+testCard = Card("Five", 5,"Spade")
 testStack = Stack()
-testStack.push(1)
-testStack.push(2)
-testStack.push(3)
-print(testStack.top())
+
+
+##Python loading json 
+f = open('cards.json')
+card = json.load(f)
+print(f"The is the amount of decks in this stack {card['amount_of_decks']}")
+##print(card['card_decks'])
+for data_item in card['card_decks']:
+    ##create a new Card for every interation and push into stack
+    card_Obj = Card(data_item['card_type'], data_item['pip_value'], data_item['suit'])
+    testStack.push(card_Obj)
+    ##print(card_Obj)
+
+##print(testStack.show())
+print(testStack.top().get_card_type())
+testStack.pop()
+testStack.pop()
+testStack.pop()
+testStack.pop()
+print(testStack.top().get_card_type())
+
+
+
+f.close()
