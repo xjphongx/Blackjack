7915ed16bfb5da0da6b711ae19f528d5a57af8c6
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-08-15 12:00:18 -0700

    Working on casino shuffle()

--- Stack.py
+++ Stack.py
@@ -9,24 +9,17 @@ class Stack:
 
     def push(self,value):
         self.stack.append(value)
-
     def pop(self):
         self.stack.pop()
-    
     def top(self):
         return self.stack[-1]
-    
     def show(self):
         return self.stack
-    
     def get_size(self): #return length of the list
         return len(self.stack)
-    
     def update_size(self): #update should be used once
         self.size = len(self.stack)
-
     def cut(self):#cut the stack in half and place one half over another
-        #print(f"Full stack size before cut {self.size}")
         tempList = []
 
         ##40% - 60% cut so it cuts randomly
@@ -36,17 +29,31 @@ class Stack:
         print(f"Cut limit {cut_limit}")
         
         for i in range(cut_limit,self.size):
-            #print(f"Value taken out {self.stack[i]}")
-            #tempList.append(i)
             tempList.append(self.stack[i])
-
-
         for j in range(0,cut_limit):
             tempList.append(self.stack[j])
-            #tempList.append(j)
-
+          
         self.stack = tempList.copy()
         
+    def casino_shuffle(self):
+        tempHighList = []
+        tempLowList = []
+
+        #put half into tempList
+        print(f"half {self.size/2} and full {self.size}")
+
+        for i in range(int(self.size/2),self.size):
+            print(i, end = " ")
+            tempHighList.append(self.stack[i])
+            #figure out how to remove from self.stack to another list 
+            
+
+        print()
+        print(f"stack {len(tempLowList)}")
+        print(f"temp {len(tempHighList)}")
+        
+            
+
         
         
         
