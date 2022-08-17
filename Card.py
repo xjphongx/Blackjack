0e6c92b8dedc11435fca45093b21b42f47802820
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-08-17 12:10:50 -0700

    add card class description

--- Card.py
+++ Card.py
@@ -1,3 +1,14 @@
+#Card Class
+#@Parameters:
+#@      card_type - Str value that represents the type of card
+#@      pip_value - Int value that represents the numeric value of the card
+#@      suit - Str value that represents what kind of suit: Spades, Clubs, Diamonds, and Hearts
+#
+#Special cases include card_type "Ace" which will add two more class variables
+#Ace cards are worth 1 or 11 depending on the current hand
+#@      low_pip_value - sets the value to be 1
+#@      high_pip_value - sets the value to be 11
+#
 
 class Card:
     def __init__(self, card_type, pip_value, suit):
