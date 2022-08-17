5e581c0494f4a64fe3a3cc27e0147a2a1b9302e0
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-08-17 12:15:04 -0700

    added card function description

--- Card.py
+++ Card.py
@@ -19,18 +19,18 @@ class Card:
             self.low_pip_value=1         #used for Aces
             self.high_pip_value=11       #used for Aces
         
-    def get_card_type(self):
+    def get_card_type(self):             #returns card type as str Example: "King"
         return self.card_type
 
-    def get_pip_value(self):
+    def get_pip_value(self):             #returns pip value as int Example: Kings are worth 10
         return self.pip_value
 
-    def get_suit(self):
+    def get_suit(self):                  #returns card's suit as str Example: "Diamonds"
         return self.suit
 
-    def get_low_pip_value(self):
+    def get_low_pip_value(self):         #return low pip value for Aces
         return self.low_pip_value
 
-    def get_high_pip_value(self):
+    def get_high_pip_value(self):        #return high pip value for Aces
         return self.high_pip_value
 
