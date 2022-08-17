dddcd4e4c3259f3820eef904a9f294be54aa9838
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-08-17 13:05:45 -0700

    corrected some typos

--- Card.py
+++ Card.py
@@ -1,13 +1,13 @@
 #Card Class
 #@Parameters:
-#@      card_type - Str value that represents the type of card
-#@      pip_value - Int value that represents the numeric value of the card
-#@      suit - Str value that represents what kind of suit: Spades, Clubs, Diamonds, and Hearts
+#@  card_type - Str value that represents the type of card
+#@  pip_value - Int value that represents the numeric value of the card
+#@  suit - Str value that represents what kind of suit: Spades, Clubs, Diamonds, and Hearts
 #
 #Special cases include card_type "Ace" which will add two more class variables
 #Ace cards are worth 1 or 11 depending on the current hand
-#@      low_pip_value - sets the value to be 1
-#@      high_pip_value - sets the value to be 11
+#@  low_pip_value - sets the value to be 1
+#@  high_pip_value - sets the value to be 11
 #
 
 class Card:
