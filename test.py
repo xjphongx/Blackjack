7f16f2a5b7d796aa0dee4e3f0b547ed4709c5531
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-08-19 22:52:04 -0700

    started an testing Hand.py

--- test.py
+++ test.py
@@ -1,34 +1,23 @@
-mainList = [5,4,5,2,1,3,5,6,7,8,5,4,3,2,3,4,5,9] #18 numbers
-tempList = []
+from Hand import Hand
+from Card import Card
 
-print("Before Algorithm")
-print(f"mainList {mainList}")    
-print(f"tempList {tempList}\n")
+#testing adding Card objects to a Hand object
 
-mainLength = int(len(mainList))
-print(f"Lengh of mainList: {len(mainList)}")
-j=int(mainLength/2)
-print(f"Half length mainList: {j}")
+testCard1 = Card("Five",5,"Hearts")
+testCard2 = Card("Jack",10,"Diamonds")
+testCard3 = Card("King",10,"Spades")
+testCard4 = Card("Three",3,"Clubs")
 
-#0-17
-#index 9 to index 18
-for i in range(int(mainLength/2), mainLength):
-    tempList.append(mainList[i])
-    #print(i, end= " ")
-    #print(j, end= " ")
+testHand = Hand()
+testHand.add_card_to_hand(testCard1)
+testHand.add_cardpip_to_sum(testCard1.pip_value)
+testHand.add_card_to_hand(testCard2)
+testHand.add_cardpip_to_sum(testCard2.pip_value)
+testHand.add_card_to_hand(testCard3)
+testHand.add_cardpip_to_sum(testCard3.pip_value)
+testHand.add_card_to_hand(testCard4)
+testHand.add_cardpip_to_sum(testCard4.pip_value)
+print(f"Sum of Current Hand: {testHand.hand_sum}")
 
-
-for j in range(int(mainLength/2), mainLength):
-    mainList.pop()
-
-print("\nAfter Algorithm")
-print(f"mainList {mainList}")    
-print(f"tempList {tempList}\n")
-
-
-
-
-
-
-
-
+for i in range(testHand.hand_size):
+    print(testHand.card_in_hand_list[i].get_card_type())
