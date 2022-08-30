9355ebede18618b0664b2b4e0da63e409912306d
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-08-29 22:34:04 -0700

    imported a button class script file from a tutorial

---
+++ button.py
@@ -0,0 +1,27 @@
+import pygame
+
+class Button():
+    def __init__(self,x, y, image, scale):
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width * scale), int(height * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+        self.clicked = False
+
+    def draw(self, surface):
+        action = False
+        mouse_position = pygame.mouse.get_pos()
+
+        #check if cursor collides with button and click condition
+        if self.rect.collidepoint(mouse_position):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked==False:
+                self.clicked = True
+                action = True
+        
+        if pygame.mouse.get_pressed()[0] == 0:
+            self.clicked = False
+
+        #draw the button on the screen
+        surface.blit(self.image, (self.rect.x, self.rect.y))
+        return action #return the action if it is pressed or not
