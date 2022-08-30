eb214c1239390db78e91de2f48fa12248727a4f4
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-08-29 22:35:25 -0700

    added a window screen and title

--- blackjack.py
+++ blackjack.py
@@ -6,15 +6,29 @@ SCREEN_WIDTH= 1300
 SCREEN_HEIGHT= 900
 
 def run_game():
+    #initialize pygame below
     pygame.init()
     
+    #Font and color
+    font = pygame.font.SysFont("arial", 80)
+    text_color = (196,196,196)
+
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     pygame.display.set_caption("Black Jack")
     clock = pygame.time.Clock()
 
+    #Start up the title menu
+    screen.fill((0,132,113)) 
+    title_image = font.render("Black Jack",True, text_color)
+    screen.blit(title_image, (459,150))    
+    print(title_image.get_width())
+
     #Game loop
     while True:
-        screen.fill((0,132,113))
+        
+
+
+
         #event handlers
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
