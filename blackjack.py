1cf9482df9298f2fa662bfe3f04efb1f90e5476b
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-08-30 23:08:11 -0700

    added menu buttons to title

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,8 @@
 import pygame
 from sys import exit
 
+from Button import Button
+
 FPS = 60
 SCREEN_WIDTH= 1300
 SCREEN_HEIGHT= 900
@@ -11,26 +13,51 @@ def run_game():
     
     #Font and color
     font = pygame.font.SysFont("arial", 80)
-    text_color = (196,196,196)
+    text_color = (225,228,230)
 
+    #Start screen
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     pygame.display.set_caption("Black Jack")
     clock = pygame.time.Clock()
+
+    
+
+    #loading button images and create button instances
+    play_game_image = pygame.image.load("images/buttons/play_game_button.png").convert_alpha()
+    play_game_button = Button(510,250, play_game_image, .15)
+    how_to_play_image = pygame.image.load("images/buttons/how_to_play_button.png").convert_alpha()
+    how_to_play_button = Button(510,350, how_to_play_image, .15)
+    quit_image = pygame.image.load("images/buttons/quit_button.png").convert_alpha()
+    quit_button = Button(510, 450, quit_image, .15)
+
 
     #Start up the title menu
     screen.fill((0,132,113)) 
     title_image = font.render("Black Jack",True, text_color)
     screen.blit(title_image, (459,150))    
-    print(title_image.get_width())
+    play_game_button.draw(screen)
+    how_to_play_button.draw(screen)
+    quit_button.draw(screen)
+
+
 
     #Game loop
     while True:
+        
         
 
 
 
         #event handlers
         for event in pygame.event.get():
+            #event for pressing keys
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:  #esc key button for pause
+                    #pause game
+                    pass
+
+            
+            
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
