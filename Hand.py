b66f71640a6151b77ce2308ee85a33f14df3f59d
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-08-31 23:32:13 -0700

    finished testing and updated Hand

--- Hand.py
+++ Hand.py
@@ -6,20 +6,24 @@ class Hand:
         self.card_in_hand_list = []
         self.hand_sum = 0
         self.hand_size = 0
-    
+
+    def update_hand(self):
+        self.hand_size = len(self.card_in_hand_list)
+
     def get_size(self):
         return self.hand_size
 
     #when adding cards to my hand, add pip value as well
-    def add_card_to_hand(self, cardToAdd): 
-        self.card_in_hand_list.append(cardToAdd)
+    def add_card_to_hand(self, card): 
+        self.card_in_hand_list.append(card)
         self.hand_size+=1       #increase hand size when adding a card
     
     def add_cardpip_to_sum(self,card_pip_value):
         self.hand_sum += card_pip_value
 
     def remove_card_from_hand(self):
-        pass
+        return self.card_in_hand_list.pop()
+        #Note: After every removal, call update_hand() to prevent index error
 
-    def subtrack_cardpip_from_sum(self):
-        pass
+    def subtrack_cardpip_from_sum(self, card_pip_value):
+        self.hand_sum -= card_pip_value
