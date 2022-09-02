98e550c111b1553073ad284e58fc3edc85758b0c
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-09-01 23:20:43 -0700

    added a add funds function

--- Player.py
+++ Player.py
@@ -4,10 +4,14 @@ from Hand import Hand
 class Player:
     def __init__(self):
         self.hand_list = Hand()
+        self.fund = 0
 
-    def add_Hand(self):#Player can add a new hand when SPLITING
+    def add_Hand(self):#Player can addS a new hand when SPLITING
         hand = Hand()
         self.hand_list.append(hand)
+    
+    def add_funds(self, fund_ammount):
+        self.fund += fund_ammount
 
 
 
