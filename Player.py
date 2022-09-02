1cb45793730810dea75ddf47390fdbe7139d91f4
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-09-02 14:19:22 -0700

    added dealer object with high starting fund count

--- Player.py
+++ Player.py
@@ -1,18 +1,27 @@
 from Hand import Hand
 
+DEALER_STARTING_FUND = 99999999999999999999999999999999999999999
+PLAYER_STARTING_FUND = 1000
 
 class Player:
-    def __init__(self):
+    def __init__(self, fund = PLAYER_STARTING_FUND):
         self.hand_list = Hand()
-        self.fund = 0
+        self.fund = fund
+        self.is_player_turn = False
+        self.player_action = None
 
     def add_Hand(self):#Player can addS a new hand when SPLITING
         hand = Hand()
         self.hand_list.append(hand)
     
-    def add_funds(self, fund_ammount):
-        self.fund += fund_ammount
+    def add_funds(self, added_amount):
+        self.fund += added_amount
+    
+    def subtract_funds(self, subtracted_amount):
+        self.fund -= subtracted_amount
 
+    def move_hand_to_discard(self):
+        pass
 
 
     ####Player Actions: Hit, Stand, Split, Double
@@ -24,3 +33,8 @@ class Player:
         pass
     def double():
         pass
+
+class Dealer(Player):
+    def __init__(self, fund = DEALER_STARTING_FUND):
+        super().__init__(fund)
+
