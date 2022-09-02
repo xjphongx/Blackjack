65f2247a16cd39518f379e0d7b4f713dcac611e4
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-09-01 23:19:29 -0700

    changed the add and remove card function to also update hand sum value

--- Hand.py
+++ Hand.py
@@ -1,12 +1,12 @@
 
-
-
 class Hand:
     def __init__(self):
         self.card_in_hand_list = []
+        self.bet_size = 0
         self.hand_sum = 0
         self.hand_size = 0
 
+    #update hand after every removal of a card to prevent index error
     def update_hand(self):
         self.hand_size = len(self.card_in_hand_list)
 
@@ -19,10 +19,12 @@ class Hand:
         self.hand_sum += card.pip_value
         self.hand_size+=1       #increase hand size when adding a card
     
-
     def remove_card_from_hand(self):
+        print(f"testing: {self.card_in_hand_list[-1]}")
+        self.hand_sum -= self.card_in_hand_list[-1].pip_value
         return self.card_in_hand_list.pop()
-        #Note: After every removal, call update_hand() to prevent index error
+         #Note: After every removal, call update_hand() to prevent index error
 
-    def subtrack_cardpip_from_sum(self, card_pip_value):
-        self.hand_sum -= card_pip_value
+    
+        
+    
