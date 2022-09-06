e1f8aa31373c9d76003350175480aa611b032ca6
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-09-06 14:39:56 -0700

    restructuring the code to be object oriented

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from scripts.blackjack import run_game
+#from scripts.blackjack import run_game
+from scripts.game import Game
 
 #Main function will intialize the game 
-def main():
-    #start the game
-    run_game()
+def main():    
+    #start the gameS
+    #run_game()
+    game = Game()
+    while game.running:
+        game.playing=True
+        game.game_loop()
+
+
+
 
 
 if __name__ == '__main__':
