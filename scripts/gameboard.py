0e18d43280f6c115927bc873eb0f3d74c96eec8d
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-09-11 23:26:44 -0700

    working on code structure of gameboard

--- scripts/gameboard.py
+++ scripts/gameboard.py
@@ -1,3 +1,4 @@
+import pygame
 from scripts.player import Dealer,Player
 from scripts.stack import Deck
 
@@ -5,31 +6,31 @@ from scripts.stack import Deck
 ## contain a stack of cards 
 
 class GameBoard():
-    def __init__(self):
+    def __init__(self,game):
         print("game board started")
+        self.game = game
         self.dealer = Dealer()
         self.deck = Deck()
+
 
     #load json png cards into the deck
     def load_cards_to_deck(self):
         pass
 
-    def play(self):
-        #play the game
-        print("playing game")
-        
+    def display_gameboard(self):
+        #set up dealer
+        self.game.screen.blit(self.game.gameboard.dealer.dealer_image_surface,self.game.gameboard.dealer.rect)           
         #set up deck 
-        
         #set up discard pile
-
         #set up player 
             #set up player's hands
-
         #start turn system
         
-
+        
+    def check_input(self):
+        pass
     
-###Testing down here
+
 
         
 
