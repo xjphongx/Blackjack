5ad1706a8f28790e3c3f138888fe6cfa82f56175
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-09-11 18:21:31 -0700

    got the card in stack to display the card image

--- scripts/secondtest.py
+++ scripts/secondtest.py
@@ -1,4 +1,4 @@
-from stack import Stack ## from Stack.py file, import and use the Stack class
+from stack import Deck,Stack ## from Stack.py file, import and use the Stack class
 from card import Card   ## from Card.py file, import and uses Card class
 
 import pygame
@@ -6,12 +6,12 @@ import json
 import sys
 
 pygame.init()
-screen = pygame.display.set_mode((500,500))
+screen = pygame.display.set_mode((1300,900)) #final dimensions
 pygame.display.set_caption('Test Window')
 clock = pygame.time.Clock()
 
-##testing area
-testStack = Stack()
+##Loading json object to Card object
+testStack = Deck()
 with open('cards.json') as jsonfile:
 #jsonfile = open('cards.json')#another way to open
     card = json.load(jsonfile)
@@ -19,21 +19,23 @@ print(f"The is the amount of decks in this stack {card['amount_of_decks']}")
 for data_item in card['card_decks']:
 ##create a new Card for every interation and push into stack
     card_Obj = Card(data_item['card_type'], data_item['pip_value'], data_item['suit'],data_item['card_image'])
+    card_Obj.card_image_surface = pygame.transform.rotozoom(card_Obj.card_image_surface,0,.2) #makes the cards smaller
     testStack.push(card_Obj)
-##ISSUE: ACES NEED TO HAVE DIFFERENT VALUES PUSHED INTO STACK
+jsonfile.close()
+
 
 print("Updating stack size")
 testStack.update_size()
 
 print("Stack before cut")
 for i in range(testStack.get_size()):
-    print(testStack.stack[i].get_card_type(),end = " ")
+    print(testStack.stack[i].card_type,end = " ")
 
 print("Initializing cut()")
 testStack.cut_deck()
 #check out the shuffle cut list
 for j in range(testStack.get_size()):
-    print(testStack.stack[j].get_card_type(), end = " ")
+    print(testStack.stack[j].card_type, end = " ")
 
 print("\nIntializing casino_shuffle()")
 testStack.casino_shuffle()
@@ -43,23 +45,19 @@ testStack.casino_shuffle()
 testStack.cut_deck()
 testStack.casino_shuffle()
 for j in range(testStack.get_size()):
-    print(testStack.stack[j].get_card_type(), end = " ")
-
-jsonfile.close()
-
-
-
-
-
-
+    print(testStack.stack[j].card_type, end = " ")
 
 
 
 while True:
+    #screen.blit(image surface, image rectangle)
+    screen.blit(testStack.stack[-1].card_image_surface,testStack.stack[-1].rect )
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+
 
     pygame.display.update()
     clock.tick(60)
