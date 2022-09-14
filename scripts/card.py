e428c2181c03699710f7c207d4eba83d6c2503c3
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-09-13 23:18:54 -0700

    updated for testing file

--- scripts/card.py
+++ scripts/card.py
@@ -3,9 +3,10 @@ import pygame
 
 #Card Class
 #@Parameters:
-#@  card_type - Str value that represents the type of card
+#@  type - Str value that represents the type of card
 #@  pip_value - Int value that represents the numeric value of the card
 #@  suit - Str value that represents what kind of suit: Spades, Clubs, Diamonds, and Hearts
+#@  image - Str value that holds the path to the image directory
 #
 #Special cases include card_type "Ace" which will add two more class variables
 #Ace cards are worth 1 or 11 depending on the current hand
@@ -14,7 +15,7 @@ import pygame
 #
 
 class Card(pygame.sprite.Sprite):
-    def __init__(self, type, pip_value, suit, card_image= None):
+    def __init__(self, type, pip_value, suit, image=None):
         super().__init__()
         self.type = type
         self.pip_value = pip_value
@@ -22,8 +23,8 @@ class Card(pygame.sprite.Sprite):
         if(type) == "Ace":
             self.low_pip_value=1         #used for Aces
             self.high_pip_value=11       #used for Aces
-        #self.card_image_surface = pygame.image.load(card_image).convert_alpha()
-        #self.rect = self.card_image_surface.get_rect(center = (1000,600))
+        #self.image_surface = pygame.image.load(image).convert_alpha()
+        #self.rect = self.image_surface.get_rect(center = (1000,600))
    
 
 
