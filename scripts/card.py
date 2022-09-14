8a87b414f5b5a23fe8b5cdc47ac716e8a3d33d2a
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-09-13 22:04:30 -0700

    adjusted the functions and class variables for testing

--- scripts/card.py
+++ scripts/card.py
@@ -14,16 +14,16 @@ import pygame
 #
 
 class Card(pygame.sprite.Sprite):
-    def __init__(self, card_type, pip_value, suit, card_image):
+    def __init__(self, type, pip_value, suit, card_image= None):
         super().__init__()
-        self.card_type = card_type
+        self.type = type
         self.pip_value = pip_value
         self.suit = suit
-        if(card_type) == "Ace":
+        if(type) == "Ace":
             self.low_pip_value=1         #used for Aces
             self.high_pip_value=11       #used for Aces
-        self.card_image_surface = pygame.image.load(card_image).convert_alpha()
-        self.rect = self.card_image_surface.get_rect(center = (1000,600))
+        #self.card_image_surface = pygame.image.load(card_image).convert_alpha()
+        #self.rect = self.card_image_surface.get_rect(center = (1000,600))
    
 
 
