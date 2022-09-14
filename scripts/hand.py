ddccb32a14cccdc7c7b37f4bdf48817b236e0097
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-09-13 22:05:56 -0700

    readjusted for testing

--- scripts/hand.py
+++ scripts/hand.py
@@ -1,33 +1,47 @@
 
 class Hand:
     def __init__(self): #add bet size when creating a hand, minimum bet
-        self.card_in_hand_list = []
-        self.bet_size = 0
+        self.card_list = []
+        self.bet_size = 50
         self.hand_sum = 0
+        self.hand_upper_sum = 0 #calculated for ace egde case
         self.hand_size = 0
         self.player_action_menu = [] #LOOK AT TUTORIAL
         #need to add a small menu containing buttons for player to press 
-
+    
+    #print out on terminal the contains of the card_list
+    def show(self):
+        for i, card in enumerate(self.card_list) :
+            print(card.type)    
+    
     #update hand after every removal of a card to prevent index error
     def update_hand(self):
-        self.hand_size = len(self.card_in_hand_list)
+        self.hand_size = len(self.card_list)
 
     #reset hand total sum to zero
     def reset_hand_sum(self):
         self.hand_sum = 0
 
     #when adding cards to my hand, add pip value as well
-    def add_card_to_hand(self, card): 
-        self.card_in_hand_list.append(card)
-        self.hand_sum += card.pip_value
+    def add_card(self, card): 
+        #set hand_sum to be the lower sum and hand uppper is uppper sum
+        if card.type == 'Ace':
+            self.hand_sum += card.low_pip_value
+            self.hand_upper_sum += card.high_pip_value
+        else:
+            self.hand_sum = card.pip_value
+            self.hand_upper_sum = card.pip_value
+
+        self.card_list.append(card) 
         self.hand_size+=1       #increase hand size when adding a card
-    
-    def remove_card_from_hand(self):
+        
+
+    def remove_card(self):
         #print(f"testing: {self.card_in_hand_list[-1]}")
-        self.hand_sum -= self.card_in_hand_list[-1].pip_value
-        return self.card_in_hand_list.pop()
+        self.hand_sum -= self.card_list[-1].pip_value
+        return self.card_list.pop()
          #Note: After every removal, call update_hand() to prevent index error
 
-    
+
         
     
--- scripts/player.py
+++ scripts/player.py
@@ -1,4 +1,4 @@
-import pygame,os
+import pygame
 from scripts.hand import Hand
 
 DEALER_STARTING_FUND = 99999999999999999999999999999999999999999
@@ -6,7 +6,7 @@ PLAYER_STARTING_FUND = 1000
 
 class Player():
     def __init__(self, fund = PLAYER_STARTING_FUND):
-        self.hand_list = Hand()
+        self.hand_list = []
         self.fund = fund
         self.is_player_turn = False
         self.player_action = None
@@ -23,7 +23,6 @@ class Player():
 
     def move_hand_to_discard(self):
         pass
-
 
     ####Player Actions: Hit, Stand, Split, Double
     def hit():
@@ -42,7 +41,8 @@ class Player():
 class Dealer(Player,pygame.sprite.Sprite):
     def __init__(self):
         super().__init__(fund = DEALER_STARTING_FUND)
-        self.dealer_image_path = os.path.abspath('images/dealer.png')
-        self.dealer_image_surface = pygame.image.load(self.dealer_image_path).convert_alpha()
-        self.dealer_image_surface = pygame.transform.rotozoom(self.dealer_image_surface,0,.2)
-        self.rect = self.dealer_image_surface.get_rect(midtop=(1300/2,20)) #screen dimension is 1300x900
+        self.hand_list = [Hand()]
+        #self.dealer_image_path = os.path.abspath('images/dealer.png')
+        #self.dealer_image_surface = pygame.image.load(self.dealer_image_path).convert_alpha()
+        #self.dealer_image_surface = pygame.transform.rotozoom(self.dealer_image_surface,0,.2)
+        #self.rect = self.dealer_image_surface.get_rect(midtop=(1300/2,20)) #screen dimension is 1300x900
