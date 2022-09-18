17de6e7ca96200f9a68ab2e54dfe307e06d5cfbb
xjphongx <jimmyphong16@csu.fullerton.edu>
2022-09-18 14:53:17 -0700

    adding intial TODOs

--- scripts/turn_system.py
+++ scripts/turn_system.py
@@ -1,4 +1,24 @@
 
-class Turn_System():
-    def __init__(self):
-        self.turn
+class TurnSystem():
+    def __init__(self, game):
+        self.game = game 
+        self.turn_list = [] #each round will cycle through this
+        self.active = False
+        
+    def intoduction(self):
+        #draw text welcoming player
+
+        #draw text explaing bet amount 
+
+        #draw text asking how many hands (defaulted testing: 1 hand)
+
+        pass
+
+    def ask_bet(self):
+        #laptop error in vegas 
+        
+        hand_amount = 2
+
+
+
+
