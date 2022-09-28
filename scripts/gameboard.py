f1861c73b60e071b40b5bcfbe5e9b45a69b5f9ab
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-09-27 20:30:03 -0700

    removed an unused function

--- scripts/gameboard.py
+++ scripts/gameboard.py
@@ -17,25 +17,6 @@ class GameBoard():
 
         
         
-    #Function display_gameboard() displays all gameboard objects
-    def initialize_gameboard(self):
-        print("starting gameboard")
-        #set up dealer
-        self.game.screen.blit(self.game.gameboard.dealer.dealer_image_surface, self.game.gameboard.dealer.rect)           
-        #set up deck
-        self.game.screen.blit(self.game.gameboard.deck_pile.deck_back_image_surface, self.game.gameboard.deck_pile.rect) 
-        #set up discard pile
-        self.game.screen.blit(self.game.gameboard.discard_pile.discard_pile_image_surface, self.game.gameboard.discard_pile.rect)
-        #set up player 
-            #set up player's hands
-            #test with one hand for one player 
-            
-
-
-
-        #start turn system
-        
-        
     def check_input(self):
         pass
     
