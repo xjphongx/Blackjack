3246f18f4a95f3e36f7e123f877a10e95288b2e6
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-10-02 23:21:15 -0700

    updating for states

--- scripts/howtoplay.py
+++ scripts/howtoplay.py
@@ -9,19 +9,21 @@ class HowToPlay(State):
         self.back_y = 55
         back_image = pygame.image.load("images/buttons/back_button.png").convert_alpha()
         self.back_button = Button(self.back_x, self.back_y, back_image, .10)
-        #TODO - add text instructions 
-        #TODO - add image instructions
-        #TODO - think about adding multiple pages/states
 
     def render(self,display):
         display.fill(self.game.background_color)
         self.game.draw_text('How to Play', 100, self.game.display_width/2, self.game.display_height/10)
-        
+        #TODO - add text instructions 
+        #TODO - add image instructions
+        #TODO - think about adding multiple pages/states
+
         #render the clickable buttons
         if self.back_button.draw(self.game.display):
-            self.game.actions["backhowtoplay"] = True
+            self.game.actions["back"] = True
             
 
     def update(self,actions):
-        if actions["backhowtoplay"]:
-    
+        #goes back to previous state by exiting current state
+        if actions["back"]:
+            self.exit_state()
+        self.game.reset_actions()
