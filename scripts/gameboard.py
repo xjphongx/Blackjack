b03aece29aa29f9a08cfd8694b278707c4f5a7c0
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-10-03 15:35:25 -0700

    add hand ring functionality

--- scripts/gameboard.py
+++ scripts/gameboard.py
@@ -7,10 +7,9 @@ from scripts.button import Button
 class Gameboard(State):
     def __init__(self, game):
         super().__init__(game)
-        self.back_x = 150
-        self.back_y = 500
+        #instantiating gameboard objects
         back_image = pygame.image.load("images/buttons/back_button.png").convert_alpha()
-        self.back_button = Button(self.back_x, self.back_y, back_image, .10)
+        self.back_button = Button(325, 50, back_image, self.game.IMAGE_SCALE)
         self.dealer = Dealer()
         self.deck_pile = DeckPile()
         self.deck_pile.load_cards_to_deck()
@@ -18,16 +17,19 @@ class Gameboard(State):
         for i in range(8):     #shuffle the deck when starting the gameboard
             self.deck_pile.cut_deck()
             self.deck_pile.casino_shuffle()
-        #Set up player input variables
-        self.input_font = pygame.font.Font(None,32)
-        self.user_text = 'test' #recieve player input
-        self.user_textbox_x = 150
-        self.user_textbox_y = 40
-        self.user_text_rect = pygame.Rect(self.game.display_width/2, 
-                                        self.game.display_height/3,
-                                        self.user_textbox_x,
-                                        self.user_textbox_y)
-        self.color = pygame.Color((255,255,255))
+        
+        #clickable hand images and curved placement       
+        hand_ring_image = pygame.image.load("images/hand_ring.png").convert_alpha()
+        hand_1_x, hand_1_y = 1150,760
+        self.hand_1_button = Button(hand_1_x,hand_1_y,hand_ring_image, scale=.25)
+        hand_2_x, hand_2_y = 900,785
+        self.hand_2_button = Button(hand_2_x,hand_2_y,hand_ring_image, scale=.25)
+        hand_3_x, hand_3_y = 650, 800 #center
+        self.hand_3_button = Button(hand_3_x,hand_3_y,hand_ring_image, scale=.25)
+        hand_4_x, hand_4_y = 400, 785
+        self.hand_4_button = Button(hand_4_x,hand_4_y, hand_ring_image, scale=.25)
+        hand_5_x, hand_5_y = 150, 760
+        self.hand_5_button = Button(hand_5_x,hand_5_y, hand_ring_image, scale=.25)
 
 
     def render(self,display):
@@ -40,14 +42,35 @@ class Gameboard(State):
         #set up discard pile
         self.game.display.blit(self.discard_pile.discard_pile_image_surface, self.discard_pile.rect)
         #self.game.draw_text('Playing game', 100, self.game.display_width/2,self.game.display_height/10)            
-        self.game.draw_text("How many hands are you playing?", 70, self.game.display_width/2,self.game.display_height/4)
+        self.game.draw_text("How many hands are you playing?", 50, self.game.display_width/2,self.game.display_height/5)
         
         #TODO set up player input
-        pygame.draw.rect(self.game.display, self.color, self.user_text_rect, 2)
-        text_surface = self.input_font.render(self.user_text, True,(255,255,255))
-        self.game.display.blit(text_surface, (self.user_text_rect.x+5,self.user_text_rect.y + 5)) #centers the text in the box
         
-        self.user_text_rect.w = max(text_surface.get_width()+10, 100) #updates the textbox to dynamicly change size 
+        if self.hand_1_button.draw(self.game.display):
+            if self.hand_1_button.isActive == False:
+                self.game.player.add_Hand()
+                self.hand_1_button.isActive = True #makes the button active once
+            
+        if self.hand_2_button.draw(self.game.display):
+            if self.hand_2_button.isActive == False:
+                self.game.player.add_Hand()
+                self.hand_2_button.isActive = True    
+
+        if self.hand_3_button.draw(self.game.display):
+            if self.hand_3_button.isActive == False:
+                self.game.player.add_Hand()
+                self.hand_3_button.isActive = True    
+
+        if self.hand_4_button.draw(self.game.display):
+            if self.hand_4_button.isActive == False:
+                self.game.player.add_Hand()
+                self.hand_4_button.isActive = True    
+            
+        if self.hand_5_button.draw(self.game.display):
+            if self.hand_5_button.isActive == False:
+                self.game.player.add_Hand()
+                self.hand_5_button.isActive = True    
+            
 
         #TODO set up the hand placement
     
