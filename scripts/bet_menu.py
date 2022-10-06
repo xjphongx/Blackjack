b9d67e427a234f7118a8a553e24534a773bfee78
xjphongx <JimmyPhong16@csu.fullerton.edu>
2022-10-06 00:14:45 -0700

    restructured code for better object management

--- scripts/bet_menu.py
+++ scripts/bet_menu.py
@@ -4,8 +4,8 @@ from scripts.button import Button
 class Bet_Menu():
     def __init__(self, game):
         self.game = game #get a reference to the game
-        self.player_fund = game.player.fund
-        self.bet_amount = 0
+        #self.player_fund = game.player.fund
+        #self.bet_amount = 0
         self.x, self.y = self.game.display_width/2 , self.game.display_height - 50
         self.bet_text_x, self.bet_text_y = self.x , self.y
         self.fund_text_x, self.fund_text_y = 1100,850
@@ -34,39 +34,39 @@ class Bet_Menu():
 
     #display all the buttons 
     def display(self):
-        self.game.draw_text(f"Bet: {self.bet_amount}", 40, self.bet_text_x, self.bet_text_y-63)
-        self.game.draw_text(f"Fund: {self.player_fund}", 40, self.fund_text_x, self.fund_text_y)
+        self.game.draw_text(f"Bet: {self.game.player.bet_amount}", 40, self.bet_text_x, self.bet_text_y-63)
+        self.game.draw_text(f"Fund: {self.game.player.fund}", 40, self.fund_text_x, self.fund_text_y)
         if self.white_chip_button.draw(self.game.display):
-            if 5 <= self.player_fund:
-                self.player_fund -= 5
-                self.bet_amount += 5
+            if 5 <= self.game.player.fund:
+                self.game.player.fund -= 5
+                self.game.player.bet_amount += 5
         if self.red_chip_button.draw(self.game.display):
-            if 10 <= self.player_fund:
-                self.player_fund -= 10
-                self.bet_amount += 10
+            if 10 <= self.game.player.fund:
+                self.game.player.fund -= 10
+                self.game.player.bet_amount += 10
         if self.blue_chip_button.draw(self.game.display):
-            if 25 <= self.player_fund:
-                self.player_fund -= 25
-                self.bet_amount += 25
+            if 25 <= self.game.player.fund:
+                self.game.player.fund -= 25
+                self.game.player.bet_amount += 25
         if self.green_chip_button.draw(self.game.display):
-            if 50 <= self.player_fund:
-                self.player_fund -= 50
-                self.bet_amount += 50
+            if 50 <= self.game.player.fund:
+                self.game.player.fund -= 50
+                self.game.player.bet_amount += 50
         if self.black_chip_button.draw(self.game.display):
-            if 100 <= self.player_fund:
-                self.player_fund -= 100
-                self.bet_amount += 100
+            if 100 <= self.game.player.fund:
+                self.game.player.fund -= 100
+                self.game.player.bet_amount += 100
         if self.yellow_chip_button.draw(self.game.display):
-            if 500 <= self.player_fund:
-                self.player_fund -= 500
-                self.bet_amount += 500
+            if 500 <= self.game.player.fund:
+                self.game.player.fund -= 500
+                self.game.player.bet_amount += 500
         if self.allin_chip_button.draw(self.game.display):
-            if self.player_fund != 0: #prevents 0 fund replacing bet amount
-                self.bet_amount += self.player_fund
-                self.player_fund = 0
+            if self.game.player.fund != 0: #prevents 0 fund replacing bet amount
+                self.game.player.bet_amount += self.game.player.fund
+                self.game.player.fund = 0
         if self.clear_button.draw(self.game.display):
-            self.player_fund += self.bet_amount
-            self.bet_amount = 0
+            self.game.player.fund += self.game.player.bet_amount
+            self.game.player.bet_amount = 0
                 
         #TODO add more functionality
 
